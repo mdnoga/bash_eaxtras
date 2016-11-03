b37b44b573e8d5b88c0daee1a8734364100fc287
mnoga <mnoga@salesforce.com>
2016-11-03 15:45:18 -0400

    Added new fresh-pi script and renames git info

---
+++ fresh-pi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# author: Michael D. Noga
+# email: mnoga@realdimensions.com
+# license: MIT
+
+echo "Getting clean PI"
+
+git checkout master
+git pull
+git fetch
+
+echo 'Done :-)'
